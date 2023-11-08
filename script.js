d642eccd3f8b78cdceca4616a1727e03b515beaa
bobur <boburmakhmadkulov05@gmail.com>
2023-11-08 21:41:49 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// let password
+// let chances = 0
+// while (password !== 9999 && chances < 3) {
+//     chances++
+//     password = +prompt("Enter your password!")
+// }
+
+
+
+let i = 1
+
+while (i <= 10) {
+    document.write(`9 * ${i} = ${9 * i} <br/>`)
+    i++
+}
+
+// do {
+//     document.write(`9 * ${i} = ${9 * i} <br/>`)
+//     i++
+// } while (i <= 10);
